7ed5372c0cff21ad97c773822cff325cd06681c1
James Turello <jturello@yahoo.com>
2016-07-19 22:42:10 -0700

    add code/specs to display Add Client page

--- app.rb
+++ app.rb
@@ -27,13 +27,11 @@ post('/stylists/new') do
   @stylist = Stylist.new({:id => nil, :name => name, :phone => phone, :location => location})
   @stylist.save
   redirect ('/')
-  # erb(:index)
 end
 
 get('/stylists/:id') do
   @page_title = "Stylist Detail Page"
   @stylist = Stylist.find(params[:id].to_i)
-# binding.pry
   erb(:stylist)
 end
 
@@ -43,37 +41,17 @@ delete('/stylists/:id') do
   redirect('/')
 end
 
-post('/stylists/:id/clients/new') do
-
-
+get('/stylists/:id/clients/new') do
+  @page_title = "Add Client Page"
+  @stylist = Stylist.find(params[:id].to_i)
+  erb(:client_form)
 end
 
-
-#
-# get('/admin') do
-#     @page_title = "Book Catalog"
-#     erb(:books)
-# end
-#
-# post('/add_book') do
-#   title = params[:book]
-#   book = Book.new({:id => nil, :title => title})
-#   book.save()
-#   erb(:books)
-# end
-#
-# get ('/books/:id') do
-#   @page_title = "Book Detail Page"
-#   @book = Book.find(params[:id].to_i)
-#   erb(:book)
-# end
-#
-# post ('/add_author') do
-#   @page_title = "Book Detail Page"
-#   @ ~~~~~ author = Author.new({:id => nil, :name => params[:author]})
-#   @author.save()
-#   @book = Book.find(params[:book_id].to_i)
-#   @book.add_authors({:author => @author})
-#   @book.authors()
-#   erb(:book)
-# end
+post('/stylists/:id/clients/new') do
+  @stylist = Stylist.find(params[:id].to_i)
+  client = Client.new({id: nil, name: params[:name], phone: params[:phone], location: params[location]})
+  client.save()
+  @stylist.add_client(client)
+  # @stylist_clients = @stylist.clients()
+  erb(:stylist)
+end
